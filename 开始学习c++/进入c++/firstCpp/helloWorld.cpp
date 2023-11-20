ad54ebb0730e6662b1ba7dfebf57b96c6be12744
tazhenwu123 <1105049762@qq.com>
2023-11-20 20:47:19 +0800

    c++是如何工作的

---
+++ 开始学习c++/进入c++/firstCpp/helloWorld.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Lenovo on 2023/11/17.
+//
+
+#include <iostream>
+
+void Log(const char* message);
+
+int main() {
+//    Log('Hello World!');
+    using namespace std;
+    cout << 'Hello Woeld' << endl;
+    cin.get();
+}
---
+++ 开始学习c++/进入c++/firstCpp/log.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Lenovo on 2023/11/20.
+//
+
+#include <iostream>
+
+void Log(const char* message) {
+    using namespace std;
+    cout << message << endl;
+}
